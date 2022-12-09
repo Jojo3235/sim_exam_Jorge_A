f3d0ccc9552eddcadf3c141bd27da390f8d9233e
Jojo3235 <joralmand@gmail.com>
2022-12-09 12:26:56 +0100

    movimiento torres

--- Hacker_chess.py
+++ Hacker_chess.py
@@ -11,19 +11,7 @@ board = np.array([['R1','N','B','Q','K','B','N','R2'],
                     ['p','p','p','p','p','p','p','p'],
                     ['r1','n','b','q','k','b','n','r2']])
 
-#Para R y r
-def RooksPosnogener(r1, r2):
-    r1 = np.where(board == r1)
-    r2 = np.where(board == r2)
-    h1 = r1[0]
-    h2 = r2[0]
-    v1 = r1[1]
-    v2 = r2[1]
-    posr1 = [h1[0], v1[0]]
-    posr2 = [h2[0], v2[0]]
-    return posr1, posr2
 
-#Rookspos for *args
 def RooksPos(*args):
     for i in args:
         i = np.where(board == i)
@@ -32,17 +20,25 @@ def RooksPos(*args):
         pos = [h[0], v[0]]
         return pos
 
-
-#definir el movimiento de la torre 
-def rookMove(r1, numero_casillas):
-    np.where(board == r1+numero_casillas)
-
 def rpos(*args):
     list = []
     for i in args:
         posr = RooksPos(i)
-        list.append('{} = {}'.format(i, posr))
+        list.append(posr)
     return list
+
+def rookMove(r1, numero_casillas):
+    r1 = np.where(board == r1)
+    h = r1[0]
+    v = r1[1]
+    pos = [h[0], v[0]]
+    if pos[0] + numero_casillas < 8:
+        board[pos[0] + numero_casillas][pos[1]] = 'R1'
+        board[pos[0]][pos[1]] = ' '
+    else:
+        print('Movimiento no válido')
+    return board
 
 if __name__ == '__main__':
     print(rpos('R1', 'R2', 'r1', 'r2'))
+    print(rookMove('r1', -3))
