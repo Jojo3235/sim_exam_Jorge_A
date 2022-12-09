60a585569d06d18c4f7978d0d5a800e177b0ae1e
Jojo3235 <joralmand@gmail.com>
2022-12-10 00:01:11 +0100

    preguntar volver a jugar minion game

--- The_minion_game.py
+++ The_minion_game.py
@@ -1,3 +1,7 @@
+VOCALES = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+SI = ['S', 's', 'Y', 'y', 'Si', 'si', 'SI', 'sI', 'Yes', 'yes', 'YES', 'yES', 'yEs', 'YeS', 'yeS']
+NO = ['N', 'n', 'No', 'no', 'NO', 'nO']
+
 def input_string():
     string = input("Introduce una sucesión de vocales y consonantes: ")
     try:
@@ -12,10 +16,10 @@ def minion_game(string):
     Stuart = 0
 
     for i in range(longitud):
-        if string[i] in 'AEIOU':
-            Kevin += longitud - i
+        if string[i] in VOCALES:
+            Kevin += (longitud - i)
         else:
-            Stuart += longitud - i
+            Stuart += (longitud - i)
 
     if Kevin > Stuart:
         print('Kevin', Kevin)
@@ -24,6 +28,26 @@ def minion_game(string):
     else:
         print('Draw')
 
+def jugar_de_nuevo():
+    respuesta = input("¿Quieres jugar de nuevo? (S/N): ")
+    try:
+        respuesta = str(respuesta)
+    except ValueError:
+        print("Error: No se ha introducido una letra o combinación de estas.")
+    if respuesta in SI:
+        return True
+    elif respuesta in NO:
+        return False
+    else:
+        print("Error: No se ha introducido una letra o combinación de estas.")
+        return False
+
+def main():
+    jugar = True
+    while jugar == True:
+        s = input_string()
+        minion_game(s)
+        jugar = jugar_de_nuevo()
+
 if __name__ == '__main__':
-    s = input_string()
-    minion_game(s)
+    main()
