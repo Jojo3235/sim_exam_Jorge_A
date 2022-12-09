98a54f24b9bde40b1bcf6cbf758d62eb1472cea3
Jojo3235 <joralmand@gmail.com>
2022-12-09 11:56:54 +0100

    mas pruebas

--- Hacker_chess.py
+++ Hacker_chess.py
@@ -12,14 +12,30 @@ board = np.array([['R1','N','B','Q','K','B','N','R2'],
                     ['r1','n','b','q','k','b','n','r2']])
 
 #Para R y r
-def verticalRooksPos(r1, r2):
+def RooksPosnogener(r1, r2):
     r1 = np.where(board == r1)
     r2 = np.where(board == r2)
     h1 = r1[0]
     h2 = r2[0]
-    return h1[0], h2[0]
+    v1 = r1[1]
+    v2 = r2[1]
+    posr1 = [h1[0], v1[0]]
+    posr2 = [h2[0], v2[0]]
+    return posr1, posr2
+
+#Rookspos for *args
+def RooksPos(*args):
+    for i in args:
+        i = np.where(board == i)
+        h = i[0]
+        v = i[1]
+        pos = [h[0], v[0]]
+        return pos
 
 
+#definir el movimiento de la torre 
+def rookMove(r1, numero_casillas):
+    np.where(board == r1+numero_casillas)
 
 if __name__ == '__main__':
-    print(verticalRooksPos('R1', 'r1'))
+    print(RooksPos('R1', 'r1'))
