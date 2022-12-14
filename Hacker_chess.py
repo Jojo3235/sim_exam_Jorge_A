00dd721eb7b6d6045827d87fe7554819c35b5deb
Jojo3235 <joralmand@gmail.com>
2022-12-15 00:20:13 +0100

    hacker chess ultimos ajustes

--- Hacker_chess.py
+++ Hacker_chess.py
@@ -1,50 +1,133 @@
-# Leer el tamaño del tablero N y la posición inicial de las torres
-N = int(input())
-pos1 = []
-pos2 = []
-for i in range(N):
-    r1, r2 = map(int, input().split())
-    pos1.append(r1)
-    pos2.append(r2)
-# Determinar si el segundo jugador tiene la ventaja inicial
-has_advantage = False
-for i in range(N):
-    # Si la torre del segundo jugador puede moverse en su primer movimiento,
-    # el segundo jugador tiene la ventaja
-    if pos2[i] < pos1[i]:
-        has_advantage = True
-        break
-# Si el segundo jugador tiene la ventaja, debe elegir la jugada óptima
-# para ganar el juego
-if has_advantage:
-    # Elegir la columna en la que la torre del segundo jugador esté
-    # más abajo que la del primer jugador
-    best_move = -1
-    max_diff = -1
-    for i in range(N):
-        diff = pos1[i] - pos2[i]
-        if diff > max_diff:
-            max_diff = diff
-            best_move = i
-    # Mover la torre del segundo jugador a la fila más alta posible
-    pos2[best_move] = N
-    # Imprimir la posición actual de las torres
-    for i in range(N):
-        print(pos1[i], pos2[i])
-else:
-    # Si el primer jugador tiene la ventaja, debe elegir la jugada óptima
-    # para ganar el juego
-    # Elegir la columna en la que la torre del primer jugador esté
-    # más abajo que la del segundo jugador
-    best_move = -1
-    max_diff = -1
-    for i in range(N):
-        diff = pos2[i] - pos1[i]
-        if diff > max_diff:
-            max_diff = diff
-            best_move = i
-    # Mover la torre del primer jugador a la fila más alta posible
-    pos1[best_move] = N
-    # Imprimir la posición actual de las torres
-    for i in range(N):
-        print(pos1[i], pos2[i])
+import numpy as np
+
+piezas = {'R': '\u265C',
+            'r': '\u2656',
+}
+
+R = piezas['R']
+r = piezas['r']
+
+def pedir_numero(cond):
+    n = int(input('Introduce un número para {}: '.format(cond)))
+    return n
+
+def pedir_numero1o2(cond):
+    n = int(input('Introduce un número entre {}: '.format(cond)))
+    while n != 1 and n != 2:
+        n = int(input('Introduce un número entre {}: '.format(cond)))
+    return n
+
+def crear_tablero(n):
+    tablero = np.empty((n,n), dtype=str)
+    for i in range(n):
+        for j in range(n):
+            tablero[i][j]=' '
+    return tablero
+
+
+def colocar_torre(n):
+    tablero = crear_tablero(n)
+    if 0<n-1:
+        for i in range(n):
+            tablero[0][i]=r
+            tablero[1][i]=R
+    else:
+        pass
+    tablero[0][0]=R
+    tablero[1][0]=' '
+    tablero[n-1][0]=r
+    return tablero
+
+#detectar la posicion de una unica torre blanca
+def seleccionar_torre_blanca(tablero):
+    posicion = []
+    n = pedir_numero("seleccionar la torre blanca")
+    for i in range(len(tablero)):
+        for j in range(len(tablero)):
+            if tablero[i][j] == R:
+                posicion.append([i,j])
+    posicion_n = posicion[n-1]
+    return posicion_n
+
+
+def seleccionar_torre_negra(tablero):
+    posicion = []
+    n = pedir_numero("seleccionar la torre negra")
+    for i in range(len(tablero)):
+        for j in range(len(tablero)):
+            if tablero[i][j] == r:
+                posicion.append([i,j])
+    posicion_n = posicion[n-1]
+    return posicion_n
+
+
+#movimiento de las torres   
+def movimiento_torre_blanca(n):
+    n = input('elegir el tamaño del tablero')
+    tablero = colocar_torre()
+    print(tablero)
+    torre = seleccionar_torre_blanca(tablero)
+    numero_casillas = pedir_numero("mover la torre")
+    h = torre[0]
+    v = torre[1]
+    pos = [h, v]
+    if pos[0] + numero_casillas < 8:
+        if tablero[pos[0]+numero_casillas][pos[1]] == ' ':
+            tablero[pos[0] + numero_casillas][pos[1]] = R
+            tablero[pos[0]][pos[1]] = ' '
+        else: 
+            return 'Movimiento no válido'
+    else:
+        return 'Movimiento no válido'
+    return tablero
+
+def movimiento_torre_blanca():
+    n = pedir_numero('elegir el tamaño del tablero')
+    tablero = colocar_torre(n)
+    print(tablero)
+    torre = seleccionar_torre_blanca(tablero)
+    numero_casillas = pedir_numero("mover la torre")
+    h = torre[0]
+    v = torre[1]
+    pos = [h, v]
+    if pos[0] + numero_casillas < n:
+        if tablero[pos[0]+numero_casillas][pos[1]] == ' ':
+            tablero[pos[0] + numero_casillas][pos[1]] = R
+            tablero[pos[0]][pos[1]] = ' '
+        else: 
+            return 'Movimiento no válido'
+    else:
+        return 'Movimiento no válido'
+    return tablero
+
+
+def movimiento_torre_negra():
+    n = pedir_numero('elegir el tamaño del tablero')
+    tablero = colocar_torre(n)
+    print(tablero)
+    torre = seleccionar_torre_negra(tablero)
+    numero_casillas = pedir_numero("mover la torre")
+    h = torre[0]
+    v = torre[1]
+    pos = [h, v]
+    if pos[0] + numero_casillas < n:
+        if tablero[pos[0]+numero_casillas][pos[1]] == ' ':
+            tablero[pos[0] + numero_casillas][pos[1]] = r
+            tablero[pos[0]][pos[1]] = ' '
+        else: 
+            return 'Movimiento no válido'
+    else:
+        return 'Movimiento no válido'
+    return tablero
+
+def main():
+    n = pedir_numero1o2("1 o 2")
+    if n == 2:
+        print(movimiento_torre_blanca())
+        print("Blancas ganan")
+    else:
+        print(movimiento_torre_negra())
+        print("Negras ganan")
+
+if __name__ == "__main__":
+    main()
